58003c3f47941b12e2e2e9d06a9411e91d7e585d
helpful-animation-4719 <um_2003_8@hotmail.com>
2019-09-30 22:24:25 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -37,7 +37,7 @@ def third_challenge
       :heroine => {
         :name => "Juliet",
         :age => "15",
-        :status => "alive"#your key/value pairs here
+        :status => "alive"
         },
       :heroine_friends => [{
         :name => "Steven",
